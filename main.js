086a2edcad1204e66bce47dfa8dc84ddd7a2621a
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-09-25 14:18:01 +0100

    feat: sponsors section - mobile

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 // Populate category list with ajax
 // implement form validation as well
 
-let url = 'https://backend.getlinked.ai/hackathon/categories-list'
+let url = 'https://backend.getlinked.ai/hackathon/categories-list';
+
+const nav = document.querySelector('.pri-nav');
+const navToggle = document.querySelector(".mob-nav-toggle");
+
+navToggle.addEventListener('click', () => {
+    const vis = nav.getAttribute("data-visible");
+
+    if (vis === "false") {
+        nav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else {
+        nav.setAttribute('data-visible',false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
